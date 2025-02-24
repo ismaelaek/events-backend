d5d23b9ec85a1d1761a0c791dcc2bca7495bc708
Ismail Ait El Kamel <97767629+ismaelaek@users.noreply.github.com>
2025-02-24 10:20:12 +0000

    chore: setup auth for different domains

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
+use Laravel\Sanctum\SanctumServiceProvider;
+
 return [
     App\Providers\AppServiceProvider::class,
+    RouteServiceProvider::class,
+    SanctumServiceProvider::class,
 ];
